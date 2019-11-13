00a7b97d02a25e33a9acd3f8088fc5490fd90a59
Amol <mailmeamol2112@gmail.com>
2019-11-12 22:09:52 -0600

    initial commit

---
+++ Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Header() {
+
+ return(
+  
+   <header> 
+     <img 
+     src = "http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+      width = "500" height = "500" alt = "Problem"
+       />
+   </header>
+ 
+ 	)
+
+}
+
+export default Header
---
+++ MemeGenerator.js
@@ -0,0 +1,109 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component{
+ 
+  constructor() {
+   
+    super()
+    this.state = {
+     
+      topText: " ",
+      bottomText: " ",
+      randomImg: "http://i.imgflip.com/1bij.jpg" ,
+      allMemeImgs: []
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  
+  componentDidMount() {
+
+    fetch("https://api.imgflip.com/get_memes")
+    .then(response => response.json())
+    .then(response => {
+    
+     const {memes} = response.data
+     //console.log(memes[0])
+     this.setState( {
+      
+      allMemeImgs : memes
+ 
+     })
+ 
+    })
+ 
+   }
+
+
+
+
+  handleChange(event) {
+  
+   const { name, value } = event.target
+   
+   this.setState({
+  
+   [name] : value
+
+    })
+  
+  
+ 
+   
+
+  }
+
+  handleSubmit(event) {
+ 
+   event.preventDefault()
+   const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+   const randMemeImg = this.state.allMemeImgs[randNum].url
+
+   this.setState({
+  
+   randomImg: randMemeImg
+
+   })
+
+
+
+  }
+
+  render(){
+
+  	 return (
+   
+    <div> 
+
+     
+      <form onSubmit = {this.handleSubmit} > 
+       
+      <input onChange = {this.handleChange} type = "text" name = "topText"  placeholder= "Top Text" value = {this.state.topText}/>
+       <input onChange = {this.handleChange} type = "text" name = "bottomText"  placeholder= "Bottom Text" value = {this.state.bottomText}/>
+
+    <button>Gen</button>
+      </form>
+
+
+
+      <div> 
+  
+    <img src = {this.state.randomImg}
+      width = "1000" height = "1000" alt = " " />
+
+      <h2> {this.state.topText} </h2> 
+      <h2> {this.state.bottomText} </h2> 
+
+      </div>
+       
+    </div>
+    
+  	 	)
+  }
+
+
+}
+
+export default MemeGenerator
